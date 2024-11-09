bd70a3e0e4ffbdddfdfcc331047d4d6cb7238f9b
kiran kumhar <kiran.kumar91@live.com>
2024-11-09 18:10:44 +0530

    find method

---
+++ findMethod.js
@@ -0,0 +1,7 @@
+//? find() : Returns the value of the element that satisfies a provided condition
+
+let numbers = [5, 12, 8, 130, 44];
+
+let found = numbers.find(num => num > 10);
+
+console.log(found);
