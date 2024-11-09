d65026ce1220fccc54e9895ecf885f5da69c9047
kiran kumhar <kiran.kumar91@live.com>
2024-11-09 18:30:12 +0530

    find index method

---
+++ findIndexMethod.js
@@ -0,0 +1,8 @@
+//? findIndex() : Return the index of the first element that satisfies a provided condition
+
+let numbers = [5, 12, 8, 130, 44];
+
+let index = numbers.findIndex(num => num > 6);
+
+
+console.log(index);
